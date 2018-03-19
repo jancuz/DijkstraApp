bcc0143147916c631bc71ee8f71e9401d6e447b1
jancuz <35611475+jancuz@users.noreply.github.com>
2018-03-19 20:35:52 +0500

    Add files via upload

---
+++ Vertex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DijkstraApp
+{
+    class Vertex
+    {
+        public string StartVertex { get; set; }         // начальная вершина
+        public string EndVertex { get; set; }           // конечная вершина
+        public double Distance  { get; set; }           // расстояние между данными вершинами
+
+        public Vertex()                                 // конструктор без параметров
+        {
+            StartVertex = null;
+            EndVertex = null;
+            Distance = double.PositiveInfinity;
+        }
+
+        public Vertex(string s, string e, double d)     // конструктор с параметрами
+        {
+            StartVertex = s;
+            EndVertex = e;
+            Distance = d;
+        }
+
+        public override string ToString()
+        {
+            return "Начальная вершина: " + StartVertex + " Конечная вершина: " + EndVertex + " Расстояние: " + Distance;
+        }
+    }
+}
